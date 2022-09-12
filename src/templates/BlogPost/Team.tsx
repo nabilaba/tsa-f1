820326c998d45ab7dccdd501b144d5c102953166
nabilaba <nabilazizbimaanggita@gmail.com>
2022-09-12 12:32:07 +0700

    fix image in blog post template

--- src/templates/BlogPost/Team.tsx
+++ src/templates/BlogPost/Team.tsx
@@ -4,28 +4,30 @@ import { About } from "../../types/HomeSections";
 import { member } from "../../content/About";
 
 const AboutPage = ({ members }: { members: About[] }) => {
-  var res = member.map((obj) => members.find((o) => o.name === obj.name) || obj);
-
   return (
     <>
       <Heading>Team Member</Heading>
       <Stack spacing="5" mt="20px">
-        {res.map((member: About) => (
-            <Member key={member.name} member={member} />
+        {members.map((obj: About, index) => (
+          <Member key={index} obj={obj} />
         ))}
       </Stack>
     </>
   );
 };
 
-const Member = ({ member }: { member: About }) => (
-  <HStack>
-    <Avatar name={member.name} src={member.image} size="md" />
+const Member = ({ obj }: { obj: About }) => (
+  <HStack spacing="4">
+    <Avatar
+      name={obj.name}
+      src={member.find((x) => x.name === obj.name)?.image}
+      size="lg"
+    />
     <Box>
       <Heading fontSize="lg" alignSelf="center">
-        {member.name}
+        {obj.name}
       </Heading>
-      <Text alignSelf="center">{member.as}</Text>
+      <Text alignSelf="center">{obj.as}</Text>
     </Box>
   </HStack>
 );
